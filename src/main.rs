789da7893565bebdced17cd4de52d97f44e56d62
Munene44 <karvinmitchar@gmail.com>
2023-11-20 15:41:35 +0300

    propmt user to enter number,read input console,parse input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
-}
+    // Prompt the user to enter a number
+    println!("Enter a number:");
+
+    // Create a mutable string to store the user input
+    let mut input = String::new();
+
+    // Read the user input from the console
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    // Parse the input into a floating-point number
+    let number: f64 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input. Please enter a number.");
+            return;
+        }
+    };
