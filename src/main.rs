aea5abe59eb172ca82fcbe87afadbad7dc2937f8
Munene44 <karvinmitchar@gmail.com>
2023-11-20 15:42:07 +0300

    remove comments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 use std::io;
 
 fn main() {
-    // Prompt the user to enter a number
+    
     println!("Enter a number:");
 
-    // Create a mutable string to store the user input
+    
     let mut input = String::new();
 
-    // Read the user input from the console
     io::stdin().read_line(&mut input).expect("Failed to read line");
 
-    // Parse the input into a floating-point number
     let number: f64 = match input.trim().parse() {
         Ok(num) => num,
         Err(_) => {
